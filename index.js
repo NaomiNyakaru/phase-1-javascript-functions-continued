22c88b4d266f5599af7c7db2af4156b9a0fb86be
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-07-16 00:56:30 +0300

    functions continued lab

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 // code your solution here
-function saturdayFun(activity = "roller-skate"){
-    return `This Saturday, I want to ${activity}!`
-   
-}
-
-function mondayWork(activity = "go to the office"){
-    return `This Monday, I will ${activity}.`
-}
-
-function wrapAdjective(sign = '*'){
-    return function(statement){
-        return `You are ${sign}${statement}${sign}!`;
-    }
-
-}
+let saturdayFun = function(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  let mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  let wrapAdjective = function(delimiter) {
+    return function(adjective) {
+      return `You are ${delimiter}${adjective}${delimiter}!`;
+    };
+  }
     
 
 
